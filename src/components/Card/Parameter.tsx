7ef48583205783e8d9aef811d3d38606a9ad4007
Olena Rubanenko <olena.rubanenko@dnv.com>
2022-12-04 22:46:37 +0200

    bonus design update

--- src/components/Card/Parameter.tsx
+++ src/components/Card/Parameter.tsx
@@ -11,10 +11,12 @@ type Props = {
 function Parameter({ title, value, bonus, accuracy } : Props) {
 	return (
 		<>
-			<Grid xs={6} sm={3}> <Typography variant="caption" color="primary"> {title} </Typography></Grid>
+			<Grid xs={6} sm={3}> <Typography variant="body1" color="primary"> {title} </Typography></Grid>
 			<Grid xs={6} sm={3}>
-				<Typography variant="body1" component="span" color="secondary"> {value.toFixed(accuracy)} </Typography>
-				{bonus ? <Typography variant="caption" color="info">+{bonus.toFixed(accuracy)}</Typography> : ''}
+				<Typography variant="body1" component="span" color="secondary">
+					{value.toFixed(accuracy)}
+					{bonus && `+${bonus.toFixed(accuracy)}`}
+				</Typography>
 			</Grid>
 		</>
 	);
